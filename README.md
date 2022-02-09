9cc5fb6281f5d3b676429bd7ccd087637dd0aa3f
josefaidt <josef.aidt@gmail.com>
2022-02-09 17:53:52 -0600

    🚀

---
+++ README.md
@@ -0,0 +1,26 @@
+# add-issue-to-projects-beta action
+
+1. to get started, create a [Personal Access Token](https://github.com/settings/tokens)
+   1. for project boards under a _user_, be sure to give the token full repo rights
+   2. for project boards under an _organization_, be sure to give read/write org permissions
+2. add the token to your repo's actions' secrets as `TOKEN`
+3. add the action
+
+```yml
+name: Add new issues to project board
+on:
+  issues:
+    types:
+      - opened
+jobs:
+  add-to-project:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Add Issue to Project
+        uses: josefaidt/add-issue-to-projects-beta
+        with:
+          project-number: 2
+          project-owner: 'josefaidt'
+        env:
+          GITHUB_TOKEN: ${{ secrets.TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,98 @@
+import { getInput, setOutput, setFailed } from '@actions/core'
+import { context } from '@actions/github'
+import { graphql } from '@octokit/graphql'
+
+const gql = String.raw
+
+const MUTATION_ADD_TO_PROJECT = gql`
+  mutation ADD_TO_PROJECT($projectId: String!, $contentId: String!) {
+    addProjectNextItem(
+      input: { projectId: $projectId, contentId: $contentId }
+    ) {
+      projectNextItem {
+        id
+      }
+    }
+  }
+`
+const QUERY_GET_PROJECT_ID_BY_NUMBER_BY_USER = gql`
+  query GET_PROJECT_ID_BY_NUMBER_BY_USER($owner: String!, $number: Int!) {
+    user(login: $owner) {
+      projectNext(number: $number) {
+        id
+      }
+    }
+  }
+`
+const GET_PROJECT_ID_BY_NUMBER_BY_ORGANIZATION = gql`
+  query GET_PROJECT_ID_BY_NUMBER_BY_ORGANIZATION(
+    $owner: String!
+    $number: Int!
+  ) {
+    organization(login: $owner) {
+      projectNext(number: $number) {
+        id
+      }
+    }
+  }
+`
+const QUERY_GET_OWNER_TYPE = gql`
+  query GET_OWNER_TYPE($owner: String!) {
+    repositoryOwner(login: $owner) {
+      __typename
+    }
+  }
+`
+
+const GITHUB_PROJECT_NUMBER = parseInt(getInput('project-number'), 10)
+const GITHUB_PROJECT_OWNER = getInput('project-owner')
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+const GITHUB_CONTENT_ID = context.payload.issue.node_id
+
+const GITHUB = {
+  User: 'User',
+  Organization: 'Organization',
+}
+
+const request = graphql.defaults({
+  headers: {
+    authorization: `token ${GITHUB_TOKEN}`,
+  },
+})
+
+try {
+  const ownerType = (
+    await request(QUERY_GET_OWNER_TYPE, { owner: GITHUB_PROJECT_OWNER })
+  )?.repositoryOwner?.__typename
+
+  const QUERY_GET_PROJECT_ID_BY_NUMBER =
+    ownerType === GITHUB.User
+      ? QUERY_GET_PROJECT_ID_BY_NUMBER_BY_USER
+      : GET_PROJECT_ID_BY_NUMBER_BY_ORGANIZATION
+
+  const projectId = (
+    await request(QUERY_GET_PROJECT_ID_BY_NUMBER, {
+      owner: GITHUB_PROJECT_OWNER,
+      number: GITHUB_PROJECT_NUMBER,
+    })
+  )?.[ownerType.toLowerCase()]?.projectNext?.id
+
+  if (!projectId) {
+    setFailed(
+      `Could not find project ID for ${GITHUB_PROJECT_OWNER}/${GITHUB_PROJECT_NUMBER}`
+    )
+    process.exit(1)
+  }
+
+  const projectItemId = (
+    await request(MUTATION_ADD_TO_PROJECT, {
+      projectId,
+      contentId: GITHUB_CONTENT_ID,
+    })
+  )?.addProjectNextItem?.projectNextItem?.id
+
+  setOutput('project-item-id', projectItemId)
+} catch (error) {
+  console.error(error)
+  setFailed(error.message)
+}
